e8c54970eab535c30488f1ce2be6feb47fb405f9
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-19 16:42:25 -0700

    add accept header

--- ajax-twitter/app/controllers/follows_controller.rb
+++ ajax-twitter/app/controllers/follows_controller.rb
@@ -9,6 +9,7 @@ class FollowsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to request.referrer }
+      format.json { render json: current_user.slice(:id, :username) }
     end
   end
 
@@ -25,6 +26,7 @@ class FollowsController < ApplicationController
       # `DELETE`.
       # See https://api.rubyonrails.org/classes/ActionController/Redirecting.html#method-i-redirect_to
       format.html { redirect_to request.referrer, status: :see_other }
+      format.json { render json: current_user.slice(:id) }
     end
   end
 end
--- ajax-twitter/app/javascript/util/api.js
+++ ajax-twitter/app/javascript/util/api.js
@@ -1,10 +1,25 @@
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
-  options.headers = {
-    // Your code here
-    ...options.headers
-  };
+    options.headers = {
+        ...options.headers,
+        "X-CSRF-Token": csrfToken,
+        'Accept': 'application/json'
+    };
 
-  return await fetch(url, options);
+    const response = await fetch(url, options);
+    
+    if(response.ok) {
+        return response.json();
+    } else {
+        throw new Error(response);
+    }
+}
+
+export function followUser(id) {
+    return customFetch(`/users/${id}/follow`, {method: 'POST'})
+}
+
+export function unfollowUser(id) {
+    return customFetch(`/users/${id}/follow`, {method: "DELETE"})
 }
