2f58309af417803ba2bc204e05f741e1671bb6c4
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-19 15:37:36 -0700

    add handleClick logic

--- ajax-twitter/app/javascript/follow_toggle.js
+++ ajax-twitter/app/javascript/follow_toggle.js
@@ -1,34 +1,43 @@
 import { API, broadcast } from "./util";
 
+
 export default class FollowToggle {
-  constructor(toggleButton) {
-    // Your code here
-  }
-
-  async handleClick(event) {
-    // Your code here
-  }
-
-  async follow() {
-    // Your code here
-  }
-
-  async unfollow() {
-    // Your code here
-  }
-
-  render() {
-    switch (this.followState) {
-      // Your code here
+    constructor(toggleButton) {
+        this.toggleButton = toggleButton;
+        toggleButton.addEventListener('click', this.handleClick.bind(this));
+        
     }
-  }
 
-  get followState() {
-    return this.toggleButton.dataset.followState;
-  }
+    async handleClick(event) {
+        event.preventDefault();
+        if (this.followState === 'followed') {
+            this.followState = 'unfollowed';
+        } else {
+            this.followState ='followed';
+        }
+        console.log(this.followState);
+    }
 
-  set followState(newState) {
-    this.toggleButton.dataset.followState = newState;
-    this.render();
-  }
+    async follow() {
+        // Your code here
+    }
+
+    async unfollow() {
+        // Your code here
+    }
+
+    render() {
+        switch (this.followState) {
+        // Your code here
+        }
+    }
+
+    get followState() {
+        return this.toggleButton.dataset.followState;
+    }
+
+    set followState(newState) {
+        this.toggleButton.dataset.followState = newState;
+        this.render();
+    }
 }
